15dd9f50936aae11c3828bd0f9fa7c76ada80212
cassandre18 <113589574+cassandre18@users.noreply.github.com>
2024-03-28 14:35:02 +0100

    Update q1.py

--- question_1/q1.py
+++ question_1/q1.py
@@ -1,1 +1,20 @@
+'importation des libraires'
+from mrjob.job import MRJob
 
+class TagsParFilm(MRJob):
+
+'fonction map'
+    def mapper(self, _, line):
+        # Ignorer len-tete
+        if line.startswith('userId'):
+            return
+        parts = line.split(',')
+        movieId = parts[1]
+        yield movieId, 1
+
+'fonction reduce'
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    TagsParFilm.run()
