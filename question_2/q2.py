c334dbfc006711d263b2c84bb4efdb0de124d5a5
cassandre18 <113589574+cassandre18@users.noreply.github.com>
2024-03-28 14:34:20 +0100

    Update q2.py

--- question_2/q2.py
+++ question_2/q2.py
@@ -1,1 +1,19 @@
+'importation des librairires'
+from mrjob.job import MRJob
 
+class TagsParUtilisateur(MRJob):
+
+'fonction map'
+    def mapper(self, _, line):
+        if line.startswith('userId'):
+            return
+        parts = line.split(',')
+        userId = parts[0]
+        yield userId, 1
+
+'fonction reduce'
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    TagsParUtilisateur.run()
